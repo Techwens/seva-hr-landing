75aa1e874eafa2861bb9597d71080ca4ab1f7779
Prity46 <132493553+Prity46@users.noreply.github.com>
2025-12-29 17:23:21 +0530

    features

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Actionform from '@/components/Actionform'
 import Features from '@/components/Features'
 import Getstarted from '@/components/Getstarted'
 import Organizations from '@/components/Organizations'
 import Powerfullplatform from '@/components/Powerfullplatform'
 import Revoluationhr from '@/components/Revolutionhr'
 import Security from '@/components/Security'
+import Worktype from '@/components/Worktype'
 import React from 'react'
 
 const Home = () => {
@@ -16,6 +18,8 @@ const Home = () => {
       <Security/>
       <Getstarted/>
       <Powerfullplatform/>
+      <Worktype/>
+      <Actionform/>
     </section>
   )
 }
